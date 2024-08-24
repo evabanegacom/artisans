cfdac2476d6853d06246e1c219f2540f38f42150
evabanegacom <udegbue69@gmail.com>
2024-08-24 22:47:37 +0100

    rearranged categories

--- src/constants/categories.tsx
+++ src/constants/categories.tsx
@@ -4,78 +4,86 @@ import { FaPalette, FaCamera, FaBook, FaFile, FaPlug, FaPaintBrush, FaGlobe, FaC
 const categories = [
   {
     id: 1,
-    name: 'DigitalArt',
-    title: 'Digital Art',
-    icon: <FaPalette />,
+    name: 'TechStartups',
+    title: 'Tech Startups',
+    icon: <FaRocket />
   },
+
   {
     id: 2,
-    name: 'Photography',
-    title: 'Photography',
-    icon: <FaCamera />,
+    name: 'Printables',
+    title: 'Printables',
+    icon: <FaGlobe />,
   },
+
   {
     id: 3,
-    name: 'E-books',
-    title: 'E-books',
-    icon: <FaBook />,
-  },
-  {
-    id: 4,
     name: 'Templates',
     title: 'Templates',
     icon: <FaFile />,
   },
+
+  {
+    id: 4,
+    name: 'ProductDesign',
+    title: 'Product Design',
+    icon: <CgWebsite />
+  },
+
   {
     id: 5,
-    name: 'Plugins',
-    title: 'Plugins',
-    icon: <FaPlug />,
+    name: 'E-books',
+    title: 'E-books',
+    icon: <FaBook />,
   },
+
   {
     id: 6,
     name: 'Themes',
     title: 'Themes',
     icon: <FaPaintBrush />,
   },
+
+ 
   {
     id: 7,
-    name: 'Printables',
-    title: 'Printables',
-    icon: <FaGlobe />,
+    name: 'DigitalArt',
+    title: 'Digital Art',
+    icon: <FaPalette />,
   },
   {
     id: 8,
-    name: 'Stickers',
-    title: 'Stickers',
-    icon: <FaCube />,
+    name: 'Photography',
+    title: 'Photography',
+    icon: <FaCamera />,
   },
+ 
   {
     id: 9,
+    name: 'Plugins',
+    title: 'Plugins',
+    icon: <FaPlug />,
+  },
+  
+  
+  // {
+  //   id: 8,
+  //   name: 'Stickers',
+  //   title: 'Stickers',
+  //   icon: <FaCube />,
+  // },
+  {
+    id: 10,
     name: 'HandCrafted',
     title: 'Hand Crafted',
     icon: <FaHandsHelping />,
   },
-  {
-    id: 10,
-    name: 'Paintings',
-    title: 'Paintings',
-    icon: <FaLayerGroup />,
-  },
-
-  {
-    id: 11,
-    name: 'WebDesign',
-    title: 'Web Design',
-    icon: <CgWebsite />
-  },
-  {
-    id: 12,
-    name: 'TechStartups',
-    title: 'Tech Startups',
-    icon: <FaRocket />
-  }
-  
+  // {
+  //   id: 10,
+  //   name: 'Paintings',
+  //   title: 'Paintings',
+  //   icon: <FaLayerGroup />,
+  // },
 ];
 
 export default categories;
